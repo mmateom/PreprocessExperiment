1c4b5fe775ee361e947b75abd2a7188ca1eb2ba6
mikel <mikel@10.1.197.111>
2018-11-30 19:11:01 +0100

    Main prepro IMU program

---
+++ Main_PreproIMU.m
@@ -0,0 +1,41 @@
+%% Get data ready for Processing
+%This is the main program to get subjects data ready to plug
+%into classification program. 
+
+%%%%%%%%%%%ONLY IMU FOR NOW
+
+%IMPORTANT:
+%1.Put subjects' names in subjectName cell.
+%2.Define the path where  you have your subjects,
+%each subject has a folder containing .mat (emg) and .txt (imu)
+%3.You'll have to select manually each subjects' folder when the dialogue
+%box pops up
+%4.If you have doubts, read each functions' definition
+%5.Enjoy!
+
+% by Mikel Mateo - University of Twente - November 2018 
+% for The BioRobotics Institute - Scuola Superiore Sant'Anna 
+%% 
+clear;clc;
+
+subjectName = {'LuisPRUEBA',...
+                'PENEGORDO'};
+           
+numSubs = numel(subjectName);           
+defpath = '/Users/mikel/Desktop/Data from GOOD experiments_1/';
+loadPeaks = 1;%set to 1 to automatically load peak locations already obtained
+              %if not, you'll have to get them manually
+%
+
+for subs = 1:numSubs            
+ %% Sync EMG with IMU 
+emgImuSync(defpath,subjectName{subs},loadPeaks);
+
+%% Preprocess IMU 
+
+preproIMU(defpath,subjectName{subs});
+end
+
+%% Put all data from all subjects in a struct
+
+data2struct(defpath);
